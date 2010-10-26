27850151ee7b395e0cfc1c644b10b7fc9267dcda
Peter Simons <simons@cryp.to>
2010-10-26 16:54:01 +0200

    Distribution/ArchLinux/CabalTranslation.hs: configure library and executable builds with --enable-shared

--- Distribution/ArchLinux/CabalTranslation.hs
+++ Distribution/ArchLinux/CabalTranslation.hs
@@ -200,7 +200,7 @@ stubPackageLibrary hkgname = emptyPkgBuild {
           "http://hackage.haskell.org/packages/archive/" ++ hkgname ++ "/${pkgver}/" ++ hkgname ++ "-${pkgver}.tar.gz"
     , arch_build =
         [ "cd ${srcdir}/" ++ hkgname ++ "-${pkgver}"
-        , "runhaskell Setup configure --prefix=/usr --docdir=/usr/share/doc/${pkgname} -O --enable-split-objs"
+        , "runhaskell Setup configure --prefix=/usr --docdir=/usr/share/doc/${pkgname} -O --enable-split-objs --enable-shared"
         , "runhaskell Setup build"
         , "runhaskell Setup haddock"
         , "runhaskell Setup register   --gen-script"
@@ -228,7 +228,7 @@ stubPackageProgram hkgname = emptyPkgBuild {
           "http://hackage.haskell.org/packages/archive/" ++ hkgname ++ "/${pkgver}/" ++ hkgname ++ "-${pkgver}.tar.gz"
     , arch_build =
         [ "cd ${srcdir}/" ++ hkgname ++ "-${pkgver}"
-        , "runhaskell Setup configure --prefix=/usr --docdir=/usr/share/doc/${pkgname} -O"
+        , "runhaskell Setup configure --prefix=/usr --docdir=/usr/share/doc/${pkgname} -O --enable-shared"
         , "runhaskell Setup build"
         ]
     , arch_package =
