6f5287273a39c80b0692bdcc0a03e446c115bd65
Peter Simons <simons@cryp.to>
2010-10-30 00:10:47 +0200

    cabal2pkgbuild.hs: fixed header handling to add a proper Maintainer comment
    There were too many functions adding '\n' to the generated output; the
    generated PKGBUILD ended up having empty lines, etc. These fixes depend
    on changes to archlinux, which need to be tested because they might
    affect cabal2arch.

--- scripts/cabal2pkgbuild.hs
+++ scripts/cabal2pkgbuild.hs
@@ -1,29 +1,26 @@
 module Main ( main ) where
 
--- package: Cabal
+import System.Environment ( getArgs )
 import Distribution.PackageDescription.Parse ( readPackageDescription )
 import Distribution.Verbosity ( normal )
-
--- package: archlinux
-import Distribution.ArchLinux.PkgBuild ( pkg2doc, arch_pkgname, pkgBody )
+import Distribution.Text ( display )
 import Distribution.ArchLinux.CabalTranslation ( preprocessCabal, cabal2pkg, install_hook_name )
 import Distribution.ArchLinux.SystemProvides ( getSystemProvidesFromFiles )
-
--- package: base
-import Text.PrettyPrint ( render )
-import System.Environment ( getArgs )
+import Distribution.ArchLinux.PkgBuild ( arch_pkgname, pkgBody )
 
 main :: IO ()
 main = do
   cabalFile:archname:release:[] <- getArgs
-  let email = "Arch Haskell Team <arch-haskell@haskell.org>"
+  let
   cabalSrc <- readPackageDescription normal cabalFile
   systemPkgs <- getSystemProvidesFromFiles "../../data/ghc-provides.txt" "../../data/library-providers.txt"
   case preprocessCabal cabalSrc systemPkgs of
     Nothing -> fail ("cannot parse and/or resolve " ++ show cabalFile)
     Just cabalPkg -> do
       let (pkgbuild, hooks) = cabal2pkg cabalPkg archname (read release) systemPkgs
-      writeFile "PKGBUILD" (render (pkg2doc email pkgbuild) ++ "\n")
+          header            = "# Maintainer: Arch Haskell Team <arch-haskell@haskell.org>\n"
+          body              = display pkgbuild ++ "\n"
+      writeFile "PKGBUILD" (header ++ body)
       case hooks of
         Just hook -> writeFile (install_hook_name (arch_pkgname (pkgBody pkgbuild))) hook
         Nothing -> return ()
