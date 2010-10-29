fc98896b98cd2b1bae9634194106d63c536e5c01
Peter Simons <simons@cryp.to>
2010-10-29 15:59:05 +0200

    cabal2pkgbuild.hs: take advantage of the generalized cabal2pkg function
    The command-line syntax of this utility changed to:
    
      cabal2pkgbuild CABALFILE PKGNAME PKGREL
    
    After successfully reading the cabal file, cabal2pkgbuild generates an
    appropriate PKGBUILD (and install file), and writes them to the current
    directory.

--- scripts/cabal2pkgbuild.hs
+++ scripts/cabal2pkgbuild.hs
@@ -7,24 +7,23 @@ import Distribution.Verbosity ( normal )
 -- package: archlinux
 import Distribution.ArchLinux.PkgBuild ( pkg2doc, arch_pkgname, pkgBody )
 import Distribution.ArchLinux.CabalTranslation ( preprocessCabal, cabal2pkg, install_hook_name )
-import Distribution.ArchLinux.SystemProvides ( getDefaultSystemProvides )
+import Distribution.ArchLinux.SystemProvides ( getSystemProvidesFromFiles )
 
 -- package: base
 import Text.PrettyPrint ( render )
 import System.Environment ( getArgs )
-import System.FilePath ( (</>) )
 
 main :: IO ()
 main = do
-  cabalFile:outputDir:[] <- getArgs
+  cabalFile:archname:release:[] <- getArgs
   let email = "Arch Haskell Team <arch-haskell@haskell.org>"
-  cabalSrc  <- readPackageDescription normal cabalFile
-  systemPkgs <- getDefaultSystemProvides
+  cabalSrc <- readPackageDescription normal cabalFile
+  systemPkgs <- getSystemProvidesFromFiles "../../data/ghc-provides.txt" "../../data/library-providers.txt"
   case preprocessCabal cabalSrc systemPkgs of
     Nothing -> fail ("cannot parse and/or resolve " ++ show cabalFile)
     Just cabalPkg -> do
-      let (pkgbuild, hooks) = cabal2pkg cabalPkg systemPkgs
-      writeFile (outputDir </> "PKGBUILD") (render (pkg2doc email pkgbuild) ++ "\n")
+      let (pkgbuild, hooks) = cabal2pkg cabalPkg archname (read release) systemPkgs
+      writeFile "PKGBUILD" (render (pkg2doc email pkgbuild) ++ "\n")
       case hooks of
-        Just hook -> writeFile (outputDir </> (install_hook_name (arch_pkgname (pkgBody pkgbuild)))) hook
+        Just hook -> writeFile (install_hook_name (arch_pkgname (pkgBody pkgbuild))) hook
         Nothing -> return ()
